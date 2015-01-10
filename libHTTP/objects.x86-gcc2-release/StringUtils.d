4c5e4a5bb05505f1179c1237fc305517f57888c9
Puck Meerburg <puck@puckipedia.nl>
2015-01-10 18:15:37 +0100

    Move stuff around

--- libHTTP/objects.x86-gcc2-release/StringUtils.d
+++
@@ -1,5 +0,0 @@
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS FOLLOW ---
-objects.x86-gcc2-release/StringUtils.o : headers/StringUtils.h \
-    headers/LibHTTPBuild.h
-
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS PRECEDE ---
--- libHTTP/objects.x86-gcc2-release/TCP_IO.d
+++
@@ -1,5 +0,0 @@
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS FOLLOW ---
-objects.x86-gcc2-release/TCP_IO.o : headers/DNSResolver.h headers/TCP_IO.h \
-    headers/LibHTTPBuild.h
-
-# --- DO NOT MODIFY THIS LINE -- AUTO-DEPENDS PRECEDE ---
